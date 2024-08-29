6669e343a063f4602b9b0f713888a8becf89f4d5
aathif12 <aathifahamad4@gmail.com>
2024-08-30 01:08:57 +0530

    Onboard

--- lib/Pages/OnBoard.dart
+++ lib/Pages/OnBoard.dart
@@ -15,59 +15,80 @@ class OneBoard extends StatelessWidget {
   }
 
   Widget buildOnboarding(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Stack(
       children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.5,
-          width: double.infinity,
-          child: CustomPaint(
-            painter: CustomShapePainter(),
-          ),
-        ),
-        const Spacer(),
         Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text(
-              "Track Your Goal",
-              style: TextStyle(
-                color: Color(0xff1d1517),
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-                height: 1.5,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  CustomPaint(
+                    painter: CustomShapePainter(),
+                    size: Size.infinite, // Fills the available space
+                  ),
+                  Center(
+                    child: Image.asset(
+                      'lib/assets/Female.png',
+                      height: 350,
+                      width: 200,
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              "Don't Worry",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xff7b6f72),
-                fontSize: 16,
-              ),
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  textAlign: TextAlign.start,
+                  "Track Your Goal",
+                  style: TextStyle(
+                    color: Color(0xff1d1517),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    height: 1.5,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Don't worry if you have trouble determining your goals, We can help you determine your goals and track your goals",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    color: Color(0xff7b6f72),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
             ),
+            const SizedBox(
+              height: 100,
+              width: 100,
+            ),
+            const Spacer(),
           ],
         ),
-        SizedBox(
-          height: 100,
-          width: 100,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            shape: const CircleBorder(),
-            padding: const EdgeInsets.all(20),
-            backgroundColor: Color(0xFF92A3FD),
-            side: BorderSide(width: 3.0, color: Colors.white),
+        Positioned(
+          bottom: 30, // Adjust the distance from the bottom
+          right: 30, // Adjust the distance from the left
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20),
+              backgroundColor: const Color(0xFF92A3FD),
+              side: const BorderSide(width: 3.0, color: Colors.white),
+            ),
+            child: const Icon(
+              Icons.keyboard_arrow_right_rounded,
+              size: 30,
+              color: Colors.white,
+            ),
           ),
-          child: const Icon(
-            Icons.keyboard_arrow_right_rounded,
-            size: 30,
-            color: Colors.white,
-          ),
         ),
-        const Spacer(),
       ],
     );
   }
@@ -86,10 +107,10 @@ class CustomShapePainter extends CustomPainter {
     final path = Path()
       ..moveTo(0, 0)
       ..lineTo(0, size.height * 0.7)
-      ..quadraticBezierTo(size.width * 0.25, size.height * 0.9,
-          size.width * 0.5, size.height * 0.8)
+      ..quadraticBezierTo(size.width * 0.3, size.height * 0.9, size.width * 0.4,
+          size.height * 0.85)
       ..quadraticBezierTo(
-          size.width * 0.75, size.height * 0.7, size.width, size.height)
+          size.width * 0.85, size.height * 0.8, size.width, size.height)
       ..lineTo(size.width, 0)
       ..close();
 
