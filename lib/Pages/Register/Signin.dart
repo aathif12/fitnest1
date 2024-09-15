7ee5b8e81598a7dd21e01e257fb7cbbddfd845e6
aathif12 <aathifahamad4@gmail.com>
2024-09-15 23:49:22 +0530

    Signin

--- lib/Pages/Register/Signin.dart
+++ lib/Pages/Register/Signin.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Signin extends StatelessWidget {
   const Signin({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,18 +14,49 @@ class Signin extends StatelessWidget {
   }
 
   Widget buildOnboarding(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return Center(
+      // Ensures everything is centered
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Centers content vertically
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // Centers content horizontally
           children: [
             Text(
-              "Hey there",
-              style: TextStyle(),
-            )
+              "Hey there,",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 10), // Adds space between the texts
+            Text(
+              "Create an Account",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Poppins-Bold",
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const SizedBox(height: 30), // Adds more space before the text field
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "UserName",
+                labelText: "Username",
+              ),
+            ),
+            const SizedBox(
+                height:
+                    20), // Adds space between the text field and other elements
+            // You can add more widgets here (e.g., password field, button)
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
