4cd3e48b4ba3d490f4c81ad155ff984d7e900e7a
aathif12 <aathifahamad4@gmail.com>
2024-09-25 22:42:19 +0530

    Paasword

--- lib/Pages/Register/Signup.dart
+++ lib/Pages/Register/Signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
 
 class Signin extends StatelessWidget {
   const Signin({super.key});
@@ -43,9 +44,15 @@ class Signin extends StatelessWidget {
             SizedBox(height: 10), // Adds more space before the text field
             TextField(
               decoration: InputDecoration(
-                fillColor: Colors.grey[300],
+                fillColor: Colors.grey[100],
+                isDense: true,
                 filled: true,
-                prefixIcon: const Icon(Icons.supervised_user_circle_sharp),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: Image.asset(
+                    'lib/assets/Profile.png',
+                  ),
+                ),
                 enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.transparent),
                     borderRadius: BorderRadius.circular(10.0)),
@@ -56,10 +63,62 @@ class Signin extends StatelessWidget {
                 labelText: "Firstname",
               ),
             ),
-            SizedBox(
-                height:
-                    20), // Adds space between the text field and other elements
-            // You can add more widgets here (e.g., password field, button)
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.grey[100],
+                isDense: true,
+                filled: true,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: Image.asset(
+                    'lib/assets/Profile.png',
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(10.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.transparent)),
+                hintText: "Lastname",
+                labelText: "Lastname",
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.grey[100],
+                isDense: true,
+                filled: true,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(top: 1),
+                  child: Image.asset(
+                    'lib/assets/Message.png',
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(10.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.transparent)),
+                hintText: "Lastname",
+                labelText: "Lastname",
+              ),
+            ),
+            SizedBox(height: 20),
+            PasswordField(
+              passwordDecoration: PasswordDecoration(),
+              passwordConstraint: r'.*[@$#.*].*',
+              border: PasswordBorder(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent))),
+            )
           ],
         ),
       ),
