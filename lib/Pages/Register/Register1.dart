41320a36fb623c5bb612c7e5bb11a77b05645f16
aathif12 <aathifahamad4@gmail.com>
2024-10-07 12:05:52 +0530

    changes

--- lib/Pages/Register/Register1.dart
+++ lib/Pages/Register/Register1.dart
@@ -1,12 +1,18 @@
-import 'package:fitnest/Pages/Intro2nd.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_elevated_button/gradient_elevated_button.dart';
 import 'package:passwordfield/passwordfield.dart';
 
 final List<String> gender = ["Male", "Female"];
 
-class Register1 extends StatelessWidget {
+class Register1 extends StatefulWidget {
   const Register1({super.key});
+
+  @override
+  _Register1State createState() => _Register1State();
+}
+
+class _Register1State extends State<Register1> {
+  String _selectedValue = "Male"; // Corrected typo and moved into state
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +43,20 @@ class Register1 extends StatelessWidget {
             "It will help us to know more about you!",
             style: TextStyle(fontWeight: FontWeight.w300),
           ),
-          DropdownButton(
-              items: gender
-                  .map((e) => DropdownMenuItem(child: Text(e), value: e))
-                  .toList(),
-              onChanged: (val) {})
+          SizedBox(
+            height: 20,
+          ),
+          DropdownButtonFormField<String>(
+            value: _selectedValue,
+            items: gender
+                .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                .toList(),
+            onChanged: (val) {
+              setState(() {
+                _selectedValue = val!;
+              });
+            },
+          ),
         ],
       ),
     );
