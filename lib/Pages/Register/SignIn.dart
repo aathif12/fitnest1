c7ad0dfa6d7381c095a001c25515db6f1f467a0b
aathif12 <aathifahamad4@gmail.com>
2024-10-09 16:46:37 +0530

    Sign

--- lib/Pages/Register/SignIn.dart
+++ lib/Pages/Register/SignIn.dart
@@ -1,5 +1,4 @@
 import 'package:fitnest/Pages/Register/GotoHome.dart';
-import 'package:fitnest/Pages/Register/Register1.dart';
 import 'package:fitnest/Pages/Register/Signup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
